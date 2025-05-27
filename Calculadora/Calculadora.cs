08b50bef23f86c17f34809bd4c559ca09bc43816
Joaquina Girbau <girbaujoaquina2@gmail.com>
2025-05-27 13:20:00 -0300

    arreglo calculadora

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -1,27 +1,37 @@
-namespace EspacioCalculadora;
-public class Calculadora
+namespace EspacioCalculadora
 {
-    private double dato;
-    public double Resultado {get => dato;}
+    public class Calculadora
+    {
+        private double dato;
+        public double Resultado { get => dato; }
 
-    public void Sumar(double Dato){
-        dato += Dato;
-    }
-    public void Restar(double Dato){
-        dato -= Dato;
-    }
-    public void Multiplicar(double Dato){
-        dato *=Dato;
-    }
-    public void Dividir(double Dato){
-        if (Dato != 0){
-            dato /= Dato;
-        }else{
-            Console.WriteLine("ERROR no se puede dividir en 0");
+        public void Sumar(double Dato)
+        {
+            dato += Dato;
         }
-    }
+        public void Restar(double Dato)
+        {
+            dato -= Dato;
+        }
+        public void Multiplicar(double Dato)
+        {
+            dato *= Dato;
+        }
+        public void Dividir(double Dato)
+        {
+            if (Dato != 0)
+            {
+                dato /= Dato;
+            }
+            else
+            {
+                Console.WriteLine("ERROR no se puede dividir en 0");
+            }
+        }
 
-    public void Limpiar(){
-        dato = 0;
+        public void Limpiar()
+        {
+            dato = 0;
+        }
     }
 }
