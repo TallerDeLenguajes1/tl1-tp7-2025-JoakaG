cda8cabf8e88ff6e70ca7ea10fffc122cd61df85
Joaquina Girbau <girbaujoaquina2@gmail.com>
2025-05-27 13:14:38 -0300

    me voy a casa

---
+++ SisPersonal/SisPersonal.cs
@@ -0,0 +1,13 @@
+namespace EspacioEmpeado;
+using System;
+public class empleado
+{
+ private string nombre;
+ private string apellido;
+ private DateTime fechaNacimiento;
+ public string EstadoCivil;
+ private DateTime fechaDeingreso;
+ public double SueldoBasico;
+ public cargos cargo; 
+}
+
